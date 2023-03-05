0b9de660a3266526899464394af1a13a035fb4bb
rimasrom <hembromsamir715@gmail.com>
2023-03-06 03:12:04 +0530

    indentation

--- main/app.js
+++ main/app.js
@@ -11,35 +11,40 @@ const questions = [
         question : `Who is the superhero that is also known as the "Man of Steel" ?
         1) Batman 
         2) Superman 
-        3) Ironman `,
+        3) Ironman \n
+        your answer : `,
         answer : "2"
     },
     {
         question : `What is Batman's real name?
            1) Robert Downey Jr
            2) Chris Evans
-           3) Bruce Wayne `,
+           3) Bruce Wayne \n
+           your answer : `,
         answer : "3"
     },
     {
         question : `What is Superman's weakness?
            1) red sun radiation
            2) magic
-           3) Kryptonite `,
+           3) Kryptonite \n
+           your answer : `,
         answer : "3"
     },
     {
         question : `How did Spider-man get his superpowers?
            1) He was bitten by a radioactive spider
            2) He ate a radioactive spider
-           3) Out of nowhere he got the power one fine morning `,
+           3) Out of nowhere he got the power one fine morning \n
+           your answer : `,
         answer : "1"
     },
     {
         question : `Which superhero has the magic lasso and bullet-proof bracelets?
            1) Natasha Romanoff
            2) Wonder Woman
-           3) Super Woman `,
+           3) Super Woman \n
+           your answer : `,
         answer : "2"
     }
 ]
