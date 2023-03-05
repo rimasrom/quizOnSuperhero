f85fdba563232b9558867ea57d484ca9f1ef6738
rimasrom <hembromsamir715@gmail.com>
2023-03-06 02:10:33 +0530

    getting username

---
+++ main/app.js
@@ -0,0 +1,8 @@
+const readlineSync = require('readline-sync')
+
+const userName = readlineSync.question("Enter your Name : ")
+console.log(`welcome ${userName}, lets play a quiz...`)
+console.log("choose an option between 1 , 2 or 3 ")
+
+
+
